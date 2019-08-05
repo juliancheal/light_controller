58af67f26f91d2eae601e1d9eb29e4afc2b8ba04
Julian Cheal <jcheal@redhat.com>
2019-08-05 17:10:04 +0100

    Hack using seperate methods, until I add params to callback

--- light_switch.py
+++ light_switch.py
@@ -15,20 +15,28 @@ landing_data_pin   = 19
 landing_switch_pin = 13
 
 
-def rotary_change(direction):
-    print("turned - " + str(direction))
+def rotary_change_hall(direction):
+    print("hall turned - " + str(direction))
     # publish.single("cmnd/kitchen/power", "off", hostname="192.168.1.38",auth={'username':"mqtt", 'password':"mqtt"})
 
 
-def switch_pressed():
-    print("button pressed")
-    publish.single("living_room/lifx/light/switch", "toggle", hostname="192.168.1.38",auth={'username':"mqtt", 'password':"mqtt"})
+def switch_pressed_hall():
+    print("hall button pressed")
+    publish.single("hallway/lifx/light/switch", "toggle", hostname="192.168.1.38",auth={'username':"mqtt", 'password':"mqtt"})
 
+def rotary_change_landing(direction):
+    print("landing turned - " + str(direction))
+    # publish.single("cmnd/kitchen/power", "off", hostname="192.168.1.38",auth={'username':"mqtt", 'password':"mqtt"})
+
+
+def switch_pressed_landing():
+    print("landing button pressed")
+    publish.single("landing/lifx/light/switch", "toggle", hostname="192.168.1.38",auth={'username':"mqtt", 'password':"mqtt"})
 
 GPIO.setmode(GPIO.BCM)
 
-hall_switch    = KY040(hall_clock_pin, hall_data_pin, hall_switch_pin, rotary_change, switch_pressed)
-landing_switch = KY040(landing_clock_pin, landing_data_pin, landing_switch_pin, rotary_change, switch_pressed)
+hall_switch    = KY040(hall_clock_pin, hall_data_pin, hall_switch_pin, rotary_change_hall, switch_pressed_hall)
+landing_switch = KY040(landing_clock_pin, landing_data_pin, landing_switch_pin, rotary_change_landing, switch_pressed_landing)
 
 hall_switch.start()
 landing_switch.start()
